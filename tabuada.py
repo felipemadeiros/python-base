29d395dd8e3d96e0f9735527aa78ab04b3405447
Felipe Madeiros <felipe.madeiros@quintoandar.com.br>
2022-06-24 17:12:29 -0300

    day 2

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Imprime a tabuada do 1 ao 10."""
+
+__version__ = "0.0.1"
+__author__ = "Felipe Madeiros"
+
+#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
+
+# Iterable (percorriveis)
+for numero in numeros:
+    print("Tabuada do:", numero)
+    for n in numeros:
+        print(numero * n)
+    print("---------------")
